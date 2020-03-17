022bb80c5c630df2ea376aff50d23fc44efd3fe6
shepherdpp <jackie_pengzhao@163.com>
2020-03-17 22:19:29 +0800

    added history util function
    added history utility function: name_change()

--- qteasy/history.py
+++ qteasy/history.py
@@ -284,6 +284,44 @@ def trade_calendar(exchange: str = 'SSE',
                          end_date=end_date,
                          is_open=is_open)
 
+def name_change(ts_code: str = None,
+                start_date: str = None,
+                end_date: str = None,
+                fields: str = None):
+    """ 历史名称变更记录
+
+    :param ts_code:
+    :param start_date:
+    :param end_date:
+    :return: pd.DataFrame
+        column              type    default     description
+        ts_code	            str	    Y	        TS代码
+        name	            str	    Y	        证券名称
+        start_date	        str	    Y	        开始日期
+        end_date	        str	    Y	        结束日期
+        ann_date	        str	    Y	        公告日期
+        change_reason	    str	    Y	        变更原因
+    example:
+    pro = ts.pro_api()
+
+    df = pro.namechange(ts_code='600848.SH',
+                        fields='ts_code,name,start_date,end_date,change_reason')
+
+    :output:
+            ts_code     name      start_date   end_date      change_reason
+    0       600848.SH   上海临港   20151118      None           改名
+    1       600848.SH   自仪股份   20070514     20151117         撤销ST
+    2       600848.SH   ST自仪     20061026    20070513         完成股改
+    3       600848.SH   SST自仪   20061009     20061025        未股改加S
+    4       600848.SH   ST自仪     20010508    20061008         ST
+    5       600848.SH   自仪股份  19940324      20010507         其他
+    """
+    if fields = None: fields = 'ts_code,name,start_date,end_date,change_reason'
+    pro = ts.pro_api()
+    return pro.namechange(ts_code=ts_code,
+                          start_date=start_date,
+                          end_date=end_date,
+                          fields=fields)
 
 # Bar price data
 # ==================
