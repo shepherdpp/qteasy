9458e271a0d89f3296d6696f4eaac655f69a736d
shepherdpp <jackie_pengzhao@163.com>
2020-09-25 23:25:04 +0800

    added test case: test_loop
    added test case: test loop
    up to now, almost all important functions have been tested, and all tested bugs in most important functions are removed.
    NOW, its time to work out a few real strategies
    more inplanted strategies will be created, and self-defined strategies will also be created in jupyter notebok type of programmes that imports qt as a real package!

--- qteasy/core.py
+++ qteasy/core.py
@@ -968,7 +968,6 @@ def _get_complete_hist(looped_value: pd.DataFrame,
 
 
 # TODO: 回测主入口函数需要增加回测结果可视化和回测结果参照标准
-# TODO: 增加一个参数，允许用户选择是否考虑现金的无风险利率增长
 # TODO: 并将过程和信息输出到log文件或log信息中，返回log信息
 def apply_loop(op_list: pd.DataFrame,
                history_list: pd.DataFrame,
