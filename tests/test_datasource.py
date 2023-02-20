5130bebd0630d044ad44dd81f6c6a9235cffb179
shepherdpp <jackie_pengzhao@163.com>
2023-02-20 23:10:21 +0800

    added live_trade.py and added framework functions
    built up framework to process trade signals and trade results during live trade
    added test cases (empty)

--- tests/test_datasource.py
+++ tests/test_datasource.py
@@ -35,6 +35,7 @@ class TestDataSource(unittest.TestCase):
         self.data_test_dir = 'data_test/'
         # 测试数据不会放在默认的data路径下，以免与已有的文件混淆
         # 使用测试数据库进行除"test_get_history_panel()"以外的其他全部测试
+        # TODO: do not explicitly leave password and user in the code
         self.ds_db = DataSource('db',
                                 host='localhost',
                                 port=3306,
