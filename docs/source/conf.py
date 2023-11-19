e079e8b0c210850b2ea3a205c99bf08262b1a24e
shepherdpp <jackie_pengzhao@163.com>
2023-11-19 23:18:00 +0800

    updated doc structure with sphinx to upload docs to readthedocs.io

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,38 +1,29 @@
 # Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'qteasy'
+copyright = '2023, Jackie PENG'
 author = 'Jackie PENG'
-
 release = '1.0'
 version = '1.0.7'
 
-# -- General configuration
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.napoleon',
-    'sphinx_rtd_theme',
-    'sphinx_markdown_tables',
-    'myst_parser',
-]
-
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
-}
-intersphinx_disabled_domains = ['std']
+extensions = ['myst_parser']
 
 templates_path = ['_templates']
+exclude_patterns = []
 
-# -- Options for HTML output
+language = 'zh'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+html_static_path = ['_static']
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,20 +1,33 @@
+.. qteasy documentation master file, created by
+   sphinx-quickstart on Sun Nov 19 21:52:20 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
 Welcome to qteasy's documentation!
-===================================
+==================================
 
 .. note::
 
    This project is under active development.
 
-Contents
---------
+
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
    getting_started
    api
-   tutorial 01 - 系统基础配置.md
+   tutorial 01 - get_started.md
    examples
    changelog
    license
    about
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
