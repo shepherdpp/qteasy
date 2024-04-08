4c9bdc34866f4459b3c0d6b5aa637811c98c6b2c
shepherdpp <jackie_pengzhao@163.com>
2024-04-08 23:05:54 +0800

    created file for tui
    a TUI is to be created for live trade mode in qteasy

---
+++ qteasy/tui.py
@@ -0,0 +1,15 @@
+# coding=utf-8
+# ======================================
+# File:     visual.py
+# Author:   Jackie PENG
+# Contact:  jackie.pengzhao@gmail.com
+# Created:  2024-04-08
+# Desc:
+#   Terminal User Interface (TUI) for
+# qteasy live trade system.
+# ======================================
+
+import os
+import sys
+from textual.app import App
+
