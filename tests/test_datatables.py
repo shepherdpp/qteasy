739971c025f3fc89c97003084bcc061beaf0d63a
shepherdpp <jackie_pengzhao@163.com>
2024-09-10 07:59:44 +0800

    added test case

---
+++ tests/test_datatables.py
@@ -0,0 +1,15 @@
+import unittest
+
+from qteasy.datatables import TABLE_MASTERS
+from qteasy.datatables import TABLE_SCHEMA
+
+
+class TestDataTables(unittest.TestCase):
+    def test_fullness(self):
+        self.assertTrue(all(tbl[0] in TABLE_SCHEMA for tbl in TABLE_MASTERS.values()))
+        self.assertEqual([tbl[0] for tbl in TABLE_MASTERS.values() if tbl[0] not in TABLE_SCHEMA],
+                         [])
+
+
+if __name__ == '__main__':
+    unittest.main()
