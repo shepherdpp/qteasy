8225cb4e8354b92669ac63d0c4b03c37c44b2586
shepherdpp <jackie_pengzhao@163.com>
2024-09-20 23:04:20 +0800

    added file datatypes.py
    this file contains definition of class DataType
    this class is defined to represent historical datatypes that can be used in qteasy

---
+++ qteasy/datatypes.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+# ======================================
+# File:     datatypes.py
+# Author:   Jackie PENG
+# Contact:  jackie.pengzhao@gmail.com
+# Created:  2024-09-20
+# Desc:
+#   Definition of DataType class,
+# representing historical data types
+# that can be used by qteasy.
+# ======================================
+
