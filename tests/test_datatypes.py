99bc8e1926f228d6bbc5de1ad52d52af837ce602
shepherdpp <jackie_pengzhao@163.com>
2024-09-26 19:10:17 +0800

    updated new type of data type map

--- tests/test_datatypes.py
+++ tests/test_datatypes.py
@@ -22,13 +22,24 @@ class TestDataTypes(unittest.TestCase):
             self.assertIsInstance(v, list)
 
             # create a new instance of the data type
-            id, freq, asset_type = k
+            name, freq, asset_type = k
+            desc = v[0]
+            acq_type = v[1]
+            kwargs = v[2]
             dtype = DataType(
-                    id=id,
+                    name=name,
                     freq=freq,
                     asset_type=asset_type,
-
+                    description=desc,
+                    acquisition_type=acq_type,
+                    **kwargs,
             )
+            self.assertIsInstance(dtype, DataType)
+            self.assertEqual(dtype.name, name)
+            self.assertEqual(dtype.freq, freq)
+            self.assertEqual(dtype.asset_type, asset_type)
+            self.assertEqual(dtype.description, desc)
+            self.assertEqual(dtype.acquisition_type, acq_type)
 
 
 if __name__ == '__main__':
