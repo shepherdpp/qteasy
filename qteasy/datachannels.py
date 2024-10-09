225c5af200855c41c33150cc36247772e35e20e0
shepherdpp <jackie_pengzhao@163.com>
2024-10-09 22:55:21 +0800

    added new file
    this new file will be used for functions that acquires historical data from other sources, sepated frrom datasource, which will be focused on storing and managing local data base, instead of acquiring data from internet sources

---
+++ qteasy/datachannels.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+# ======================================
+# File:     datachannels.py
+# Author:   Jackie PENG
+# Contact:  jackie.pengzhao@gmail.com
+# Created:  2024-10-09
+# Desc:
+#   Definition of series of functions to
+# acquire or donwload from different
+# sources such as tushare, yahoo finance,
+# etc. historical data that can be stored
+# in the datasource.
+# ======================================
+
