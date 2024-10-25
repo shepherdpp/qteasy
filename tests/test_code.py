a93623c577f80a8500323ac33d7a42da9931634c
shepherdpp <jackie_pengzhao@163.com>
2024-10-25 23:54:36 +0800

    testing qt_codes

--- tests/test_code.py
+++ tests/test_code.py
@@ -30,16 +30,51 @@ class TestCode(unittest.TestCase):
 
         code = '000001'
         qt_code = QtCode(code)
+        print(qt_code)
+        self.assertEqual(qt_code, '000001.SZ')
+        self.assertEqual(qt_code.market, 'SZ')
+        self.assertEqual(qt_code.symbol, '000001')
+        self.assertEqual(qt_code.asset_type, 'E')
+
         code = '000001.SH'
         qt_code = QtCode(code)
+        print(qt_code)
+        self.assertEqual(qt_code, '000001.SH')
+        self.assertEqual(qt_code.market, 'SH')
+        self.assertEqual(qt_code.symbol, '000001')
+        self.assertEqual(qt_code.asset_type, 'IDX')
+
         code = '600001'
         qt_code = QtCode(code)
+        print(qt_code)
+        self.assertEqual(qt_code, '600001.SH')
+        self.assertEqual(qt_code.market, 'SH')
+        self.assertEqual(qt_code.symbol, '600001')
+        self.assertEqual(qt_code.asset_type, 'E')
+
         code = '0045'
         qt_code = QtCode(code)
+        print(qt_code)
+        self.assertEqual(qt_code, '0045.HK')
+        self.assertEqual(qt_code.market, 'HK')
+        self.assertEqual(qt_code.symbol, '0045')
+        self.assertEqual(qt_code.asset_type, 'E')
+
         code = 'MSFT'
         qt_code = QtCode(code)
+        print(qt_code)
+        self.assertEqual(qt_code, 'MSFT.US')
+        self.assertEqual(qt_code.market, 'US')
+        self.assertEqual(qt_code.symbol, 'MSFT')
+        self.assertEqual(qt_code.asset_type, 'E')
+
         code = 'AAPL'
         qt_code = QtCode(code)
+        print(qt_code)
+        self.assertEqual(qt_code, 'AAPL.US')
+        self.assertEqual(qt_code.market, 'US')
+        self.assertEqual(qt_code.symbol, 'AAPL')
+        self.assertEqual(qt_code.asset_type, 'E')
 
 
 if __name__ == '__main__':
