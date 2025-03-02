1bb389f582ca9ebc31f6a84aecc60c7a40802a4a
shepherdpp <jackie_pengzhao@163.com>
2025-03-02 22:24:09 +0800

    updated doc folder

---
+++ docs/conf.py
@@ -0,0 +1,55 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# import os
+# import sys
+#
+# sys.path.insert(0, os.path.abspath('../'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'qteasy'
+copyright = '2023, Jackie PENG'
+author = 'Jackie PENG'
+version = '1.4'
+release = '1.4.8'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+source_parsers = {
+    '.md': 'recommonmark.parser.CommonMarkParser',
+}
+
+source_suffix = ['.rst', '.md']
+
+extensions = [
+    'myst_parser',
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.viewcode',
+    # 'sphinx-markdown-checkbox',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+# html_logo = "qteasy_logo_horizontal.png"
+html_logo = "QTEASY_facebook_cover.png"
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
