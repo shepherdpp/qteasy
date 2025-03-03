0a02a5150b11416eb014b3cc9f32972a57e30e5d
shepherdpp <jackie_pengzhao@163.com>
2025-03-03 20:14:32 +0800

    updated document file structure

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,10 +3,10 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# import os
-# import sys
-#
-# sys.path.insert(0, os.path.abspath('../'))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -40,7 +40,9 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'zh'
+language = 'zh_CN'
+locale_dirs = ['locale/']
+gettext_compact = False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
