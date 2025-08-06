262decf58a699ab16fae2f4bc387ed013ca4052b
shepherdpp <jackie_pengzhao@163.com>
2025-08-06 21:08:30 +0800

    started to updated group.py

--- qteasy/group.py
+++ qteasy/group.py
@@ -16,17 +16,32 @@
 import numpy as np
 from qteasy.strategy import BaseStrategy
 
+from qteasy.blender import (
+    blender_parser,
+    signal_blend,
+    human_blender,
+)
+
 
 class Group:
     def __init__(self, name: str, signal_type: str = 'PT', blender: str = None):
 
         self.name = name
         self.signal_type = signal_type
-        self.blender = blender
+        self.blender_str = blender
+        self._blender = blender_parser(blender)
 
         self.members = []
         self.run_timing = None
         self.run_freq = None
+
+    @property
+    def member_strageties(self):
+        return [strategy.name for strategy in self.members]
+
+    @property
+    def blender(self):
+        return human_blender(self.blender_str, self.members)
 
     def add_strategy(self, strategy: BaseStrategy):
 
@@ -43,16 +58,12 @@ class Group:
                 raise ValueError(f"Strategy {strategy.name} has a different run frequency than the group {self.name}.")
             self.members.append(strategy)
 
-    def set_group_data_window(self, data_windows: dict):
-        """Set the data buffers for the group."""
-        for member in self.members:
-            member.update_data_window(data_windows)
-
     def blend(self, signals: np.ndarray):
         """Set the blender for the group."""
-        method = self.blender
-        print(f"blended({signals}) with method {method}")
-        return np.sum(signals) / 2
+        return signal_blend(op_signals=signals, blender=self._blender)
+
+    def human_blender(self):
+        return human_blender(self.blender_str, self.members)
 
     def __repr__(self):
         return f"Group(name={self.name}, members={self.members})"
