3d06cf25faf8424a24ae995177ba065d977b0133
shepherdpp <jackie_pengzhao@163.com>
2025-08-11 22:06:20 +0800

    started to update class Group and Operator

---
+++ tests/test_group.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+# ======================================
+# File:     test_group.py
+# Author:   Jackie PENG
+# Contact:  jackie.pengzhao@gmail.com
+# Created:  2025-08-11
+# Desc:
+#   Unittest for all strategy group
+# properties and methods.
+# ======================================
+
+import unittest
+
+
+class TestGroup(unittest.TestCase):
+    def test_creation(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_properties(self):
+        self.assertEqual(True, False)
+
+    def test_add_strategy(self):
+        self.assertEqual(True, False)
+
+    def test_merge(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
